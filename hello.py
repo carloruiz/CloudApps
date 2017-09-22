48f8f287e9b71d0d3449633d5813473558c426a2
ttc2132 <ttc2132@columbia.edu>
2017-09-22 18:36:41 -0400

    added example query code

--- hello.py
+++ hello.py
@@ -20,5 +20,31 @@ app = Flask(__name__)
 def hello():
     return "Welcome to Python Flask App!"
 
+
+# Example query. Makes an array out of last_name field in northwind db. 
+  cursor = g.conn.execute("SELECT last_name FROM employees")
+  last_names = []
+  for result in cursor:
+    last_names.append(result['last_name'])
+  cursor.close()
+
+'''This runs before every web request'''
+@app.before_request
+def before_request():
+  try:
+    g.conn = engine.connect()
+  except:
+    print("uh oh, problem connecting to database")
+    import traceback; traceback.print_exc()
+    g.conn = None
+
+'''tears down db connection. details are in sqlalcemy documentation'''
+@app.teardown_request
+def teardown_request(exception):
+  try:
+    g.conn.close()
+  except Exception as e:
+    pass
+    
 if __name__ == "__main__":
     app.run()
