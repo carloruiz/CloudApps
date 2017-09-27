3178530df42ccc6eb251d195cdc589d22fad4599
deemedadream <tta.casey@gmail.com>
2017-09-26 23:35:27 -0400

    put address url in the body of response

--- person_constants.py
+++ person_constants.py
@@ -1,2 +1,6 @@
 
 DATABASEURI = 'mysql+pymysql://user1:2cloud4grp@northwind.cpqu2popccu7.us-east-2.rds.amazonaws.com:3306/Persons'
+
+persons_endpoint = 'http://persons2-env.us-east-2.elasticbeanstalk.com/person'
+
+address= 'http://addressesv2-env.us-east-2.elasticbeanstalk.com/address'
